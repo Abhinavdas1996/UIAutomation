bde39d8b3e073865935878a13c7d1b17398fa9d6
Abhinavdas1996 <abhinavdas3005@gmail.com>
2023-01-07 18:38:02 +0530

    Auto validate login in web url

---
+++ src/main/java/pass/Demo.java
@@ -0,0 +1,25 @@
+package pass;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","/Users/abhinavdas/Documents/Driver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
+        driver.findElement(By.id("inputUsername")).sendKeys("Rahul");
+        driver.findElement(By.name("inputPassword")).sendKeys("12345");
+        driver.findElement(By.id("chkboxOne")).click();
+        driver.findElement(By.name("chkboxTwo")).click();
+        driver.findElement(By.className("submit")).click();
+        System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+
+        driver.quit();
+    }
+}
